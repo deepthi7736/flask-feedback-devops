134172e69995a940bd788073a4e9c806d72e6605
Deepthi mol Haridas <deeepteeei@gmail.com>
2026-02-18 13:30:46 +0530

    Initial commit - Flask Feedback CRUD App

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db():
+    conn = sqlite3.connect("feedback.db")
+    return conn
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form["name"]
+        message = request.form["message"]
+
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO feedback (name, message) VALUES (?, ?)", (name, message))
+        conn.commit()
+        conn.close()
+
+        return redirect("/view")
+
+    return render_template("index.html")
+
+@app.route("/view")
+def view():
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM feedback")
+    data = cur.fetchall()
+    conn.close()
+
+    return render_template("view.html", feedback=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
